2b67db61868af282a83bedcd3b0bc3ac2fe6b9ff
Đào Xuân Tân <128350618+tandao0909@users.noreply.github.com>
2024-06-16 22:02:39 +0700

    Add documentation for CleanProcess.py, remove drop_columns functions for conciseness

--- EDA_FE/CleanProcess.py
+++ EDA_FE/CleanProcess.py
@@ -1,9 +1,15 @@
 import pandas as pd
 
-def handle_lat_long(df):
+def handle_lat_long(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Drop duplicates and NaN values from the 'Latitude' and 'Longitude' columns from the passed DataFrame.
+
+    Parameters:
+        df (pandas.DataFrame): The input DataFrame
+
+    Returns:
+        pandas.DataFrame: The DataFrame with duplicates and NaN values removed from the 'Latitude' and 'Longitude' columns
+    """
     df.drop_duplicates(subset=['Latitude', 'Longitude'],inplace = True)  
     df.dropna(subset=['Latitude', 'Longitude'], inplace = True)
     return df
-
-def drop_columns(df, columns_to_drop):
-    return df.drop(columns=columns_to_drop, axis = 1, inplace = True)
