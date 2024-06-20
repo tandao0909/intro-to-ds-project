dd8196681a25c6c18f89e455482e08ca75e599dc
Đào Xuân Tân <128350618+tandao0909@users.noreply.github.com>
2024-06-20 10:54:16 +0700

    Revert back to the documented version, change the output of VisualizeData to pd.DataFrame

--- EDA_FE/Pipeline_Housing.py
+++ EDA_FE/Pipeline_Housing.py
@@ -1,106 +1,256 @@
+"""
+Provides wrapper estimator classes for functions in the CleanProcess.py, VisualizeLog.py, and OutliersScale.py files.
+"""
+
 import numpy as np
 import pandas as pd
+from folium import Map
+
 from sklearn.preprocessing import StandardScaler
-from CleanProcess import handle_lat_long, drop_columns
-from VisualizeLog import check_coordinates_in_vietnam, RealEstateVisualizer, visualize_clusters # for visualizing the data
-from OutliersScale import drop_outliers, apply_log_transformation, convert_boolean_to_numeric, apply_floor_and_compute_rooms, compute_features, transform_housing_data, scale_data
 from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.preprocessing import FunctionTransformer
+
+from CleanProcess import handle_lat_long
+from VisualizeLog import check_coordinates_in_vietnam, visualize_clusters # for visualizing the data
+from OutliersScale import drop_outliers, transform_housing_data
 
 # Handle latitude and longitude
 class HandleLatLong(BaseEstimator, TransformerMixin):
+    """
+    Handle Latitude and Longitude attributes
+    """
     def __init__(self):
         pass
-    def fit(self, X, y=None):
+
+    def fit(self, X:pd.DataFrame, y=None):
         return self
-    def transform(self, X):
-        handle_lat_long(X)
+    
+    def transform(self, X:pd.DataFrame) -> pd.DataFrame:
+        """
+        Drop duplicates and NaN values from the 'Latitude' and 'Longitude' columns from the passed DataFrame.
+
+        Parameters:
+            X (pandas.DataFrame): The input DataFrame
+
+        Returns:
+            pandas.DataFrame: The DataFrame with duplicates and NaN values removed from the 'Latitude' and 'Longitude' columns
+        """
+        X = handle_lat_long(X)
         return X
     
-# Custom transformer to drop columns
 class DropColumns(BaseEstimator, TransformerMixin):
+    """
+    Drop columns from the DataFrame
+
+    Parameters:
+        columns (list): The list of columns to drop
+
+    Returns:
+        pandas.DataFrame: The DataFrame with the specified columns dropped
+    """
     def __init__(self, columns):
         self.columns = columns
-    def fit(self, X, y=None):
+
+    def fit(self, X:pd.DataFrame, y=None):
         return self
-    def transform(self, X):
-        return X.drop(self.columns, axis=1)
     
-# Check whether or not the coordinates are in Vietnam
+    def transform(self, X:pd.DataFrame) -> pd.DataFrame:
+        """
+        Drop columns from the DataFrame
+
+        Parameters:
+            X (pandas.DataFrame): The input DataFrame
+
+        Returns:
+            pandas.DataFrame: The DataFrame with the specified columns dropped
+        """
+        return X.drop(self.columns, axis=1)
+   
 class CheckCoordinatesInVietnam(BaseEstimator, TransformerMixin):
-    def __init__(self, shapefile_path):
+    """
+    Check if the coordinates are in Vietnam
+
+    Parameters:
+        shapefile_path (str): The path to the shapefile
+
+    Returns:
+        pandas.DataFrame: The DataFrame with the coordinates checked
+    """
+    def __init__(self, shapefile_path:str):
         self.shapefile_path = shapefile_path
+
     def fit(self, X, y=None):
         return self
-    def transform(self, X):
-        check_coordinates_in_vietnam(shapefile_path=self.shapefile_path, housing_df=X)
+    
+    def transform(self, X:pd.DataFrame) -> pd.DataFrame:
+        X = check_coordinates_in_vietnam(shapefile_path=self.shapefile_path, housing_df=X)
         return X
     
-# Visualize the data
 class VisualizeData(BaseEstimator, TransformerMixin):
+    """
+    Visualize the data
+
+    Returns:
+        pandas.DataFrame: The DataFrame with the data visualized
+    """
     def __init__(self):
         pass
+
     def fit(self, X, y=None):
         return self
-    def transform(self, X):
+    def transform(self, X:pd.DataFrame) -> pd.DataFrame:
+        """
+        Visualize the data
+
+        Parameters:
+            X (pandas.DataFrame): The input DataFrame
+        
+        Returns:
+            folium.Map: The map with the data visualized
+        """
+        # We don't need to return the map, just visualize it
         visualize_clusters(X)
         return X
     
-# Custom transformer to apply log transformation
 class ApplyLogTransformation(BaseEstimator, TransformerMixin):
-    def __init__(self, columns):
+    """
+    Apply log transformation to specific columns of a DataFrame
+
+    Parameters:
+        columns (list): The columns to apply log transformation
+
+    Returns:
+        pandas.DataFrame: The DataFrame with log transformation applied to specific columns
+    """
+
+    def __init__(self, columns:list[str]):
         self.columns = columns
     def fit(self, X, y=None):
         return self
-    def transform(self, X):
+    def transform(self, X:pd.DataFrame) -> pd.DataFrame:
+        """
+        Apply log transformation to specific columns of a DataFrame
+
+        Parameters:
+            X (pandas.DataFrame): The input DataFrame
+
+        Returns:
+            pandas.DataFrame: The DataFrame with log transformation applied to specific columns
+        """
         for column in self.columns:
             X[column] = np.log(X[column])
         return X
     
-# Custom transformer to drop outliers
 class DropOutliers(BaseEstimator, TransformerMixin):
-    def __init__(self, column):
+    """
+    Drop outliers from the DataFrame using the box plot method
+
+    Parameters:
+        column (str): The column to drop outliers from  
+
+    Returns:
+        pandas.DataFrame: The DataFrame without outliers
+    """
+    
+    def __init__(self, column:str):
         self.column = column
     def fit(self, X, y=None):
         return self
-    def transform(self, X):
+    def transform(self, X:pd.DataFrame) -> pd.DataFrame:
+        """
+        Drop outliers from the DataFrame using the box plot method
+
+        Parameters:
+            X (pandas.DataFrame): The input DataFrame
+
+        Returns:
+            pandas.DataFrame: The DataFrame without outliers
+        """
         X = drop_outliers(X, self.column)
         return X
     
-# Custom transformer to convert boolean to numeric
 class ConvertBooleanToNumeric(BaseEstimator, TransformerMixin):
-    def __init__(self, columns):
+    """
+    Convert boolean columns to numeric
+
+    Parameters:
+        columns (list[str]): The columns to convert
+
+    Returns:
+        pandas.DataFrame: The DataFrame with boolean columns converted to numeric
+    """
+    def __init__(self, columns:list[str]):
         self.columns = columns
+
     def fit(self, X, y=None):
         return self
-    def transform(self, X):
+    
+    def transform(self, X:pd.DataFrame) -> pd.DataFrame:
+        """
+        Convert boolean columns to numeric
+
+        Parameters:
+            X (pandas.DataFrame): The input DataFrame
+
+        Returns:
+            pandas.DataFrame: The DataFrame with boolean columns converted to numeric
+        """
         for column in self.columns:
             X[column] = X[column].astype(int)
         return X
 
-# Custom transformer to transform housing data
 class TransformHousingData(BaseEstimator, TransformerMixin):
+    """
+    Transform the housing data
+
+    Returns:
+        pandas.DataFrame: The DataFrame with the housing data transformed
+    """
     def __init__(self):
         pass
+
     def fit(self, X, y=None):
         return self
-    def transform(self, X):
+    
+    def transform(self, X:pd.DataFrame) -> pd.DataFrame:
+        """
+        Transform the housing data
+
+        Parameters:
+            X (pandas.DataFrame): The input DataFrame
+
+        Returns:
+            pandas.DataFrame: The DataFrame with the housing data transformed
+        """
         X = transform_housing_data(X)
         return X
 
-# Standardize the data
 class StandardizeData(BaseEstimator, TransformerMixin):
+    """
+    Standardize the data
+
+    Returns:
+        pandas.DataFrame: The DataFrame with the data standardized
+    """
     def __init__(self):
         pass
+
     def fit(self, X, y=None):
         return self
-    def transform(self, X):
+    
+    def transform(self, X:pd.DataFrame) -> pd.DataFrame:
+        """
+        Standardize the data
+
+        Parameters:
+            X (pandas.DataFrame): The input DataFrame
+
+        Returns:
+            pandas.DataFrame: The DataFrame with the data standardized
+        """
         scaler = StandardScaler()
-        X = scaler.fit_transform(X)
+        values = scaler.fit_transform(X)
+        X = pd.DataFrame(values, columns=X.columns, index=X.index)
         return X
     
 # Pipeline for numerical attributes
